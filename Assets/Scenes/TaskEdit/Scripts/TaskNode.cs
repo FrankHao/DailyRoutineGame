008e51fc7a4c85ec6be597a61fa278d8951f3cfd
FrankHao <foanagh@gmail.com>
2019-01-05 16:26:07 +1100

    Changed task scene

--- Assets/Scenes/TaskEdit/Scripts/TaskNode.cs
+++ Assets/Scenes/TaskEdit/Scripts/TaskNode.cs
@@ -7,7 +7,40 @@ namespace KidsTodo.Task
 
     public class TaskNode : MonoBehaviour
     {
+        private float distance = 5f;
 
+        [SerializeField]
+        protected GameObject taskName;
+        public string TaskName
+        {
+            get
+            {
+                return taskName.GetComponent<TextMesh>().text;
+            }
+            set
+            {
+                taskName.GetComponent<TextMesh>().text = value;
+            }
+        }
+
+        /// <summary>
+        /// On mouse drag function.
+        /// </summary>
+        private void OnMouseDrag()
+        {
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            transform.position = objPosition;
+        }
+
+        /// <summary>
+        /// On mouse enter function.
+        /// </summary>
+        private void OnMouseEnter()
+        {
+            Debug.Log("OnMouseEnter");
+        }
     }
 }
 
