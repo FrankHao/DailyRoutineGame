903b576455d364e979c1f6ef576b0e8ac70662c1
Mao <Jerry.Mao@scientificgames.com>
2019-01-23 13:25:55 +1100

    Temporary commit for resolving csrf issue and make the networking works. Will update to JWT later.

--- Assets/Common/Network/NetworkManager.cs
+++ Assets/Common/Network/NetworkManager.cs
@@ -115,32 +115,13 @@ namespace KidsTodo.Common.Network
 
         public IEnumerator PostRequest(string uri, string bodyJsonString, Action<ResultMessage> onResponse)
         {
-            /* UnityWebRequest loginPage = UnityWebRequest.Get(uri);
-             yield return loginPage.SendWebRequest();
-             if (loginPage.isNetworkError || loginPage.isHttpError)
-             {
-                 Debug.Log(loginPage.error);
-                 yield break;
-             }
-
-             // get the csrf cookie
-             string SetCookie = loginPage.GetResponseHeader("set-cookie");
-             Debug.Log(SetCookie);
-             Regex rxCookie = new Regex("csrftoken=(?<csrf_token>.{64});");
-             MatchCollection cookieMatches = rxCookie.Matches(SetCookie);
-             string csrfCookie = cookieMatches[0].Groups["csrf_token"].Value;
-
-             // get the middleware value
-             string loginPageHtml = loginPage.downloadHandler.text;
-             Regex rxMiddleware = new Regex("name='csrfmiddlewaretoken' value='(?<csrf_token>.{64})'");
-             MatchCollection middlewareMatches = rxMiddleware.Matches(loginPageHtml);
-             string csrfMiddlewareToken = middlewareMatches[0].Groups["csrf_token"].Value;*/
-
 
             UnityWebRequest request = UnityWebRequest.Put(uri, bodyJsonString);
             request.method = "POST";
+            request.SetRequestHeader("Cookie", "");
             request.SetRequestHeader("Content-Type", "application/json");
             request.SetRequestHeader("X-UNITY-METHOD", "POST");
+            UnityWebRequest.ClearCookieCache();
             yield return request.SendWebRequest();
 
             var headers = request.GetResponseHeaders();
